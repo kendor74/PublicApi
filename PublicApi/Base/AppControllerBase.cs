c98af06406ff4fe0ee3da20dd3d8f7581440ff54
kendor <103393540+kendor74@users.noreply.github.com>
2026-07-01 15:43:22 +0300

    Add files via upload

---
+++ PublicApi/Base/AppControllerBase.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using Public.Application.Base;
+
+namespace Public.Api.Base
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AppControllerBase : ControllerBase
+    {
+        protected ObjectResult CustomResult<T>(BaseResponse<T> response)
+        {
+            return response.StatusCode switch
+            {
+                HttpStatusCode.OK => Ok(response),
+                HttpStatusCode.Created => Created(string.Empty, response),
+                HttpStatusCode.BadRequest => BadRequest(response),
+                HttpStatusCode.Unauthorized => Unauthorized(response),
+                HttpStatusCode.NotFound => NotFound(response),
+                HttpStatusCode.UnprocessableEntity => UnprocessableEntity(response),
+                _ => BadRequest(response)
+            };
+        }
+
+        
+    }
+}
